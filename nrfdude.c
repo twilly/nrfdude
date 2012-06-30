ea158fc4f328ac7043895f2db540c248b7a161aa
Ruben Breimoen <ruben.breimoen@gmail.com>
2012-06-30 12:37:08 +0200

    Deleted config.mk, added device_id of nRFgo to the code

--- nrfdude.c
+++ nrfdude.c
@@ -28,6 +28,7 @@
 #define DEVSTRNAME          "nRF24LU1+"
 #define VENDOR_NORDIC       0x1915
 #define PID_NRF24LU         0x0101
+#define PID_NRFGO           0x001a
 #define FLASH_SIZE          32768
 #define IN                  0x81
 #define OUT                 0x01
