9d4afc1dcd06894f9782ffcdfa30b179c9f30341
Tristan Willy <tristan.willy@gmail.com>
2012-07-29 21:09:18 -0700

    Add copyright headers.

--- nrfgo.c
+++ nrfgo.c
@@ -1,6 +1,6 @@
 /* nrfgo.c
  *
- * Copyright (C) 2012
+ * Copyright (C) 2012 Ruben Breimoen
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
